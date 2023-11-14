ae58c2830939a9bb10587948d397dede5d57c597
Eric Jang <eric.jang@databricks.com>
2023-11-14 13:19:28 -0800

    test

--- gradio/app/gradio_app.py
+++ gradio/app/gradio_app.py
@@ -2,7 +2,7 @@ import random
 import gradio as gr
 
 def random_response(message, history):
-    return random.choice(["Yes", "No"])
+    return random.choice(["Yes", "No", "Maybe"])
 
 demo = gr.ChatInterface(random_response)
 
